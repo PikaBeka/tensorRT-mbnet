d1f897c712cf81e9f6ce584edd288edddaa6c60a
PikaBeka <beka.kalmahanbet@gmail.com>
2022-04-11 23:04:50 +0600

    Finished Conv filter and maxError function

--- conv.cu
+++ conv.cu
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 
+#define WEIGHT_SIZE 5
 class Layer
 {
 public:
@@ -17,11 +18,22 @@ Layer::~Layer()
 
 __global__ void kernel_conv_filter(float *input, float *pre_output, float *weight)
 {
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = threadIdx.y;
+    int col = threadIdx.x;
 
-    float sum = 0.0;
-    float value = 0.0;
+    float temp = 0;
+
+    for (int i = 0; i < WEIGHT_SIZE; i++)
+    {
+        for (int j = 0; j < WEIGHT_SIZE; j++)
+        {
+            int input_idx = (row + i) * 28 + (col + j);
+            int block_idx = blockIdx.x * 25 + i * WEIGHT_SIZE + j;
+            temp += input[input_idx] * weight[block_idx];
+        }
+    }
+    int idx = blockIdx.x * 576 + row * 24 + col;
+    pre_output[idx] = temp;
 }
 
 __global__ void kernel_conv_bias(float *pre_output, float *bias)
@@ -33,12 +45,31 @@ __global__ void kernel_conv_sigmoid(float *pre_output, float *output)
     int idx = blockIdx.x * 576 + 24 * threadIdx.x + threadIdx.y;
 
     output[idx] = 1.0 / (1.0 + exp(-pre_output[idx]));
+}
 
-    printf("%f \n", output[idx]);
+// Inputs are: arr - array, val - asseted value, size will be dimx * dimy * dimz
+void maxError(float *arr, float val, float size)
+{
+    float maxErr = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] != val)
+        {
+            maxErr = max(maxErr, abs(arr[i] - val));
+        }
+    }
+    printf("maxEror = %f (asserted with %f)\n", maxErr, val);
 }
 
 void forward_pass(double data[28][28])
 {
+    int THREADS = 24;
+    int BLOCKS = 6;
+    dim3 numBlocks(BLOCKS, BLOCKS);
+    dim3 threadPerBlock(THREADS, THREADS);
+    kernel_conv_filter<<<numBlocks, threadPerBlock>>>(d_input, d_output, d_weight);
+
+    kernel_conv_sigmoid<<<6, dim3(24, 24)>>>()
 }
 
 int main()
