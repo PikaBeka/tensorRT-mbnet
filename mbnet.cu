a594edbf235da135ccd04980519adb5b254347d6
PikaBeka <beka.kalmahanbet@gmail.com>
2024-06-10 22:42:06 +0500

    fix threads count

--- mbnet.cu
+++ mbnet.cu
@@ -1314,8 +1314,8 @@ void pass(int argc, char **argv)
         int k = input_channels * RS * RS;
         int n = K;
 
-        dim3 dimBlock(TILE_SIZE, TILE_SIZE, 1);
-        dim3 dimGrid((m + TILE_SIZE) / TILE_SIZE, (n + TILE_SIZE) / TILE_SIZE);
+        dim3 dimBlock(TILE_SIZE, TILE_SIZE);
+        dim3 dimGrid((m + TILE_SIZE - 1) / TILE_SIZE, (n + TILE_SIZE - 1) / TILE_SIZE);
 
         gemm_shared_kernel<<<dimGrid, dimBlock>>>(im2col_A, gemm_B, d_output, m, k, n);
 
