2d652a5c26164b214d3cb01fcbd858ef3cc0c097
PikaBeka <beka.kalmahanbet@gmail.com>
2024-08-03 18:02:31 +0600

    gst

--- autotesting.sh
+++ autotesting.sh
@@ -3,7 +3,7 @@
 out_path=(unroll_cublass tensorrt)
 metrics=(sm_efficiency achieved_occupancy warp_execution_efficiency inst_per_warp gld_efficiency gst_efficiency shared_efficiency shared_utilization
            l2_utilization global_hit_rate tex_cache_hit_rate tex_utilization ipc inst_issued inst_executed issue_slot_utilization dram_utilization)
-metric=shared_utilization
+metric=gst_efficiency
 
 for j in ${!out_path[@]}; do
 	./tester.sh ${out_path[$j]} ${metric}
