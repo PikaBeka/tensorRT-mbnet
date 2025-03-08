f48a5eed8827ef830f140fc8ba6fcc32d544e4be
PikaBeka <beka.kalmahanbet@gmail.com>
2025-03-08 22:18:31 +0400

    fix autotesting script

--- autotesting.sh
+++ autotesting.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 out_path=(mbnet_method)
-metrics=(sm_efficiency achieved_occupancy warp_execution_efficiency inst_per_warp gld_efficiency gst_efficiency shared_efficiency shared_utilization
-           l2_utilization global_hit_rate tex_cache_hit_rate tex_utilization ipc inst_issued inst_executed issue_slot_utilization dram_utilization)
+metrics=(
+	None
+	# sm_efficiency achieved_occupancy warp_execution_efficiency inst_per_warp gld_efficiency gst_efficiency shared_efficiency shared_utilization
+	# l2_utilization global_hit_rate tex_cache_hit_rate tex_utilization ipc inst_issued inst_executed issue_slot_utilization dram_utilization
+)
 # metric=(stall_sync stall_other stall_not_selected stall_memory_throttle stall_memory_dependency stall_inst_fetch stall_exec_dependency stall_constant_memory_dependency)
 
 for j in ${!out_path[@]}; do
-	# for i in ${!metric[@]}; do
-	./tester.sh ${out_path[$j]}
-	sleep 10
-	# done
+	for i in ${!metric[@]}; do
+		./tester.sh ${out_path[$j]} ${metrics[$i]}
+		sleep 10
+	done
 done
